ef8e8c2a8d7dac5a3c897cc86f733b53f5e1e477
oh geun taek <rmsxor94@naver.com>
2016-12-26 21:09:08 +0900

    Initial Commit

---
+++ BJ11055/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BJ11055
+//
+//  Created by 오근택 on 2016. 12. 26..
+//  Copyright © 2016년 오근택. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
